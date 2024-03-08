5781616bc0e90c7841fd24d9ddb0e37be27f3a2a
stonek <amit-yurist@mail.ru>
2024-03-08 16:43:21 +0300

    some fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
 
@@ -11,13 +11,13 @@ const bot = new TelegramBot(token, {polling: true});
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+// app.use(cors());
+//
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
